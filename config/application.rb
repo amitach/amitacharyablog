0074c09e9773ba9280f9abae5b2fb5687c007f07
Amit Acharya <amit.acharta@infibeam.net>
2013-09-29 20:25:26 +0530

    memcache store for cache

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Blog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.cache_store = :mem_cache_store, "localhost"
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
